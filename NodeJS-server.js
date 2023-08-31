14fbe823ce558d87ed39a7bc7020beab71964411
matheezmujahid <143556031+matheezmujahid@users.noreply.github.com>
2023-08-30 21:59:29 -0400

    Update NodeJS-server.js

--- NodeJS-server.js
+++ NodeJS-server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello!, this is nodejs server file');
+    res.send('Hello!, this is nodejs server code');
 });
 
 
